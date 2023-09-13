44febc938c8533eb3fd0452474ee6d4f351e4279
Preethik06 <preethikerkar06@gmail.com>
2023-09-13 15:30:38 +0530

    Comit3

--- app/src/main/java/com/example/final2/MainActivity.java
+++ app/src/main/java/com/example/final2/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.final2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView logout = (ImageView) findViewById(R.id.logout);
+        auth=FirebaseAuth.getInstance();
+        user= auth.getCurrentUser();
+        if(user==null){
+            Intent intent=new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
